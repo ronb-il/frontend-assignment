54f241917fac74079c91ded999c8a701b7bf0dee
ronb-il <rbuenavida.il@gmail.com>
2017-07-27 00:04:21 +0300

    * using sass preprocessor for styles * simple javascript bundle for gulp

--- assets/javascript/components/dashboard/dashboard-index.component.js
+++ assets/javascript/components/dashboard/dashboard-index.component.js
@@ -1,8 +1,22 @@
-<main>
+
+var dashboard = {
+  template: `
+  <main>
     <div class="content">
         <dashboard-webtype data="$ctrl.initialData.ClearWeb" name="ClearWeb">
         </dashboard-webtype>
         <dashboard-webtype data="$ctrl.initialData.DarkWeb" name="DarkWeb">
         </dashboard-webtype>
     </div>
-</main>
+  </main>
+  `,
+  controller: DashboardController,
+  bindings: {
+    initialData: '<',
+    data: '<',
+    options: '<'
+  }
+};
+
+angular.module('root')
+  .component('dashboard', dashboard);
--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,41 @@ var concat = require('gulp-concat');
 var notify = require('gulp-notify');
 var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
-var cssnano = require('gulp-cssnano');
+var cssnano = require('cssnano');
 var sass = require('gulp-ruby-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var rename = require('gulp-rename');
+var atImport = require('postcss-import');
+var postcss = require('gulp-postcss');
+var cssnext = require('postcss-cssnext');
+var path = require('path');
 
-gulp.task('scripts', function() {
-  return gulp.src(['public/app/**/*.js'])
-    .pipe(sourcemaps.init())
+/**
+ * The output directory for all the built files.
+ */
+const DEST = './public/dist';
+
+/**
+ * The name of the Github repo.
+ */
+const REPO = 'insights-assignment';
+
+/**
+ * The base public path of the site.
+ */
+const PUBLIC_PATH = path.join('/', (isProd() ? REPO : '.'), '/');
+
+function isProd() {
+  return process.env.NODE_ENV == 'production';
+}
+
+gulp.task('js', function() {
+  return gulp.src(['assets/javascript/**/*.js'])
+    // .pipe(sourcemaps.init())
     .pipe(concat('bundle.js'))
     .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop()) 
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('public/dist/scripts/'))
+    // .pipe(sourcemaps.write())
+    .pipe(gulp.dest(DEST))
     .pipe(notify({message : 'JS files successfully concated and reduced'}));
 });
 
@@ -25,14 +48,33 @@ gulp.task('jshint', function() {
     .pipe(jshint.reporter('default'));
 });
 
-gulp.task('styles', function(){
-  return sass('public/css/**/*.scss', { style: 'expanded'})
+
+gulp.task('css', function () {
+    return gulp.src('./assets/css/main.css')
+    .pipe(concat('bundle.css'))
+    .pipe(postcss([
+      atImport(),
+      cssnext({}),
+      cssnano()
+    ]))
+    .pipe(gulp.dest(DEST))
+    .pipe(notify({message : 'Scss Successfully compiled and reduced'}));
+});
+
+/*
+gulp.task('css', function(){
+  return sass('assets/css/main.css', { 
+      style: 'expanded', 
+      includePaths: ['node_modules'] }
+    )
+    .pipe(plumber({errorHandler: streamError}))
     .pipe(gulp.dest('public/dist/styles/'))
     .pipe(rename({suffix : '.min'}))
     .pipe(cssnano())
     .pipe(concat('style.css'))
     .pipe(notify({message : 'Scss Successfully compiled and reduced'}));
 });
+*/
 
 gulp.task('watch', function() {
   // Watch .scss files
